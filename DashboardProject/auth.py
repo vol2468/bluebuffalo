4aa375d80e98f5888eae7bb6e3d3b32470a85bc9
leolelion <leokaiya@proton.me>
2024-03-14 11:35:33 -0700

    more fixes

--- DashboardProject/auth.py
+++ DashboardProject/auth.py
@@ -7,32 +7,37 @@ auth = Blueprint('auth', __name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+
 @auth.route('/header')
 def header():
     return render_template("header.html")
+
 
 @auth.route('/index')
 def index():
     return render_template("index.html")
 
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
     return render_template("login.html")
+
 
 @auth.route('/newAccount')
 def newAccount():
     return render_template("newAccount.html")
 
+
 @auth.route('/accountSetting')
 def accountSetting():
     return render_template("accountSetting.html")
+
 
 @auth.route('/map')
 def map():
     return render_template("map.html")
 
+
 @auth.route('/analysis')
 def analysis():
     return render_template('analysis.html')
-
