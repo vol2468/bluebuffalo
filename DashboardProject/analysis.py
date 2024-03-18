7881b8c3764bd67fa4496b225bb5ee5771f0c5f5
joyfuluj <joyumejiego@yahoo.com>
2024-03-18 00:16:41 -0700

    complete time-series graph

--- DashboardProject/analysis.py
+++ DashboardProject/analysis.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request
-import json
 # import pandas as pd
 # import seaborn as sns
 # import matplotlib.pyplot as plt
-from markupsafe import Markup
 import pandas as pd
 
 analysis = Flask(__name__) 
@@ -20,8 +18,22 @@ def perform_analysis():
     # Convert the date column to datetime format
     df['Date'] = pd.to_datetime(df['Date'])
 
-    filtered_df = df[df['City'] == city]
-    
-    mean_values = filtered_df[['O3 Mean', 'CO Mean', 'SO2 Mean', 'NO2 Mean']].mean().values.tolist()
+    filtered_df_2000 = df[(df['City'] == city) & (df['Date'] == '2000-01-01')]
+    filtered_df_2004 = df[(df['City'] == city) & (df['Date'] == '2004-01-01')]
+    filtered_df_2008 = df[(df['City'] == city) & (df['Date'] == '2008-01-01')]
+    filtered_df_2012 = df[(df['City'] == city) & (df['Date'] == '2010-01-01')]
+    filtered_df_2016 = df[(df['City'] == city) & (df['Date'] == '2014-01-01')]
+    filtered_df_2020 = df[(df['City'] == city) & (df['Date'] == '2018-01-01')]
+    filtered_df_2021 = df[(df['City'] == city) & (df['Date'] == '2021-01-01')]
 
-    return render_template("analysis.html", meanData=mean_values, city=city)
+    # Time-series
+    total_values_list = [
+        filtered_df_2000[['O3 Mean', 'CO Mean', 'SO2 Mean', 'NO2 Mean']].values.sum(),
+        filtered_df_2004[['O3 Mean', 'CO Mean', 'SO2 Mean', 'NO2 Mean']].values.sum(),
+        filtered_df_2008[['O3 Mean', 'CO Mean', 'SO2 Mean', 'NO2 Mean']].values.sum(),
+        filtered_df_2012[['O3 Mean', 'CO Mean', 'SO2 Mean', 'NO2 Mean']].values.sum(),
+        filtered_df_2016[['O3 Mean', 'CO Mean', 'SO2 Mean', 'NO2 Mean']].values.sum(),
+        filtered_df_2020[['O3 Mean', 'CO Mean', 'SO2 Mean', 'NO2 Mean']].values.sum(),
+        filtered_df_2021[['O3 Mean', 'CO Mean', 'SO2 Mean', 'NO2 Mean']].values.sum()
+    ]
+    return render_template("analysis.html", city=city, total=total_values_list)
