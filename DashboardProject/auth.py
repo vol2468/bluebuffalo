e755219f6bd503b768ddd6abcf39f79dacd1b948
leolelion <leokaiya@proton.me>
2024-03-21 11:21:19 -0700

    cleaning in progress work from new account verification branch

--- DashboardProject/auth.py
+++ DashboardProject/auth.py
@@ -29,27 +29,7 @@ def login():
 
 @auth.route('/newAccount', methods=['GET', 'POST'])
 def newAccount():
-    if request.method == 'POST':
-        firstname = request.form.get('fname')
-        lastname = request.form.get('lname')
-        email = request.form.get('regEmail')
-        password1 = request.form.get('pass1')
-        password2 = request.form.get('pass2')
-
-        if len(email) < 4:
-            flash('Email is invalid.', category='error')
-        elif password1 != password2:
-            flash('Passwords do not match.', category='error')
-        elif len(password1) < 10:
-            flash('Passwords must be at least 10 characters.', category='error')
-        else:
-            new_user = User(email=email, firstname=firstname, lastname=lastname, password=generate_password_hash(password1, method='shf22'))
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Account creation successful!', category='success')
-            return redirect(url_for('views.home'))
-    else:
-        return render_template("newAccount.html")
+    return render_template("newAccount.html")
 
 
 @auth.route('/accountSetting')
