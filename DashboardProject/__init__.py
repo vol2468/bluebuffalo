0b7442a05acb4265e03a87791cde1a7fb2378774
joyfuluj <joyumejiego@yahoo.com>
2024-03-26 16:22:51 -0700

    updated

--- DashboardProject/__init__.py
+++ DashboardProject/__init__.py
@@ -41,47 +41,47 @@ def create_database(app):
         with app.app_context():
             db.create_all()
             # Read data from CSV and insert into tables
-            # insert_data_from_csv()
+            insert_data_from_csv()
         print("Created Database!")
 
 
-# def insert_data_from_csv():
-#     from .models import City, Pollutant
-#     # Read data from your CSV file (adjust the filename as needed)
-#     csv_filename = r'C:\Users\leosc\Documents\GitHub\bluebuffalo\Data\processed\pollution.csv'
-#     df = pd.read_csv(csv_filename)
+def insert_data_from_csv():
+    from .models import City, Pollutant
+    # Read data from your CSV file (adjust the filename as needed)
+    csv_filename = r'C:\Users\leosc\Documents\GitHub\bluebuffalo\Data\processed\pollution.csv'
+    df = pd.read_csv(csv_filename)
 
-#     # Create SQLAlchemy session
-#     Session = sessionmaker(bind=db.engine)
-#     session = Session()
+    # Create SQLAlchemy session
+    Session = sessionmaker(bind=db.engine)
+    session = Session()
 
-#     try:
-#         for index, row in df.iterrows():
-#             date_obj = datetime.strptime(row['Date'], '%Y-%m-%d')
-#             # Create City record
-#             city_record = City(cityName=row['City'],
-#                                population=row['Population (at 2000)'],
-#                                latitude=row['Latitude'],
-#                                longitude=row['Longitude'])
-#             session.add(city_record)
+    try:
+        for index, row in df.iterrows():
+            date_obj = datetime.strptime(row['Date'], '%Y-%m-%d')
+            # Create City record
+            city_record = City(cityName=row['City'],
+                               population=row['Population (at 2000)'],
+                               latitude=row['Latitude'],
+                               longitude=row['Longitude'])
+            session.add(city_record)
 
-#             # Create Pollutant record
-#             pollutant_record = Pollutant(city=city_record,
-#                                          date=date_obj,
-#                                          O3Mean=row['O3 Mean'],
-#                                          O3AQI=row['O3 AQI'],
-#                                          COMean=row['CO Mean'],
-#                                          COAQI=row['CO AQI'],
-#                                          SO2Mean=row['SO2 Mean'],
-#                                          SO2AQI=row['SO2 AQI'],
-#                                          NO2Mean=row['NO2 Mean'],
-#                                          NO2AQI=row['NO2 AQI'])
-#             session.add(pollutant_record)
+            # Create Pollutant record
+            pollutant_record = Pollutant(city=city_record,
+                                         date=date_obj,
+                                         O3Mean=row['O3 Mean'],
+                                         O3AQI=row['O3 AQI'],
+                                         COMean=row['CO Mean'],
+                                         COAQI=row['CO AQI'],
+                                         SO2Mean=row['SO2 Mean'],
+                                         SO2AQI=row['SO2 AQI'],
+                                         NO2Mean=row['NO2 Mean'],
+                                         NO2AQI=row['NO2 AQI'])
+            session.add(pollutant_record)
 
-#         session.commit()
-#         print("Data inserted successfully!")
-#     except Exception as e:
-#         session.rollback()
-#         print(f"Error inserting data: {str(e)}")
-#     finally:
-#         session.close()
+        session.commit()
+        print("Data inserted successfully!")
+    except Exception as e:
+        session.rollback()
+        print(f"Error inserting data: {str(e)}")
+    finally:
+        session.close()
--- DashboardProject/analysis.py
+++ DashboardProject/analysis.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import sqlite3
-from DashboardProject.models import Pollutant, City
-from DashboardProject import db
-from sqlalchemy import func
 
 # Connection to database
 conn = sqlite3.connect('instance/database.db')
@@ -24,7 +21,7 @@ def perform_analysis():
     
     mean_values = filtered_df[['O3 Mean', 'CO Mean', 'SO2 Mean', 'NO2 Mean']].mean().values.tolist()
 
-      # Time-series
+
     filtered_df_2000 = df[(df['City'] == city) & (df['Date'] == '2000-01-01')]
     filtered_df_2004 = df[(df['City'] == city) & (df['Date'] == '2004-01-01')]
     filtered_df_2008 = df[(df['City'] == city) & (df['Date'] == '2008-01-01')]
@@ -50,32 +47,13 @@ def perform_analysis():
     
     # ACTUAL CODE
     # latitude, longitude = get_coordinates(city)
-    # mean_values = get_mean_values(city)
 
     return render_template("analysis.html", meanData=mean_values, city=city, total=total_values_list, lat=latitude, long=longitude)
-# Getting corrdinate of specific city from database
+
 def get_coordinates(city_name):
-    city = City.query.filter_by(name=city_name).first()
-    if city:
-        return city.latitude, city.longitude
+    cursor.execute("SELECT latitude, longitude FROM cities WHERE name=?", (city_name,))
+    row = cursor.fetchone()
+    if row:
+        latitude, longitude = row
+        return latitude, longitude
     return None, None
-
-# Getting mean value from database
-def get_mean_values(city_name):
-    mean_values = db.session.query(
-        db.func.avg(Pollutant.O3_Mean),
-        db.func.avg(Pollutant.CO_Mean),
-        db.func.avg(Pollutant.SO2_Mean),
-        db.func.avg(Pollutant.NO2_Mean)
-    ).filter_by(cityName=city_name).first()
-
-    return mean_values
-
-# Getting total values of pollutant in each year from database
-def get_total_mean(city_name):
-    total_values_list = []
-    for year in ['2000', '2004', '2008', '2012', '2016', '2020', '2021']:
-        total_sum = db.session.query(func.sum(Pollutant.O3_Mean), func.sum(Pollutant.CO_Mean), func.sum(Pollutant.SO2_Mean), func.sum(Pollutant.NO2_Mean)) \
-                        .filter_by(cityName=city_name, date=f'{year}-01-01').first()
-        total_values_list.append(sum(total_sum))
-    return total_values_list
