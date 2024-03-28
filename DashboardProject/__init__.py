154127e34c759aabb2782a805ca94875d877722b
joyfuluj <joyumejiego@yahoo.com>
2024-03-28 11:22:32 -0700

    fixed comment section

--- DashboardProject/__init__.py
+++ DashboardProject/__init__.py
@@ -32,7 +32,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    create_database(app)
+    # create_database(app)
 
     return app
 
--- DashboardProject/insertComment.py
+++ DashboardProject/insertComment.py
@@ -6,8 +6,8 @@ from DashboardProject import db
 def insert_comment():
     city = request.form.get('city')
     comment = request.form.get('comment')
-    cId = City.query.filter_by(cityName=city).first()
-    new_comment = Comment(pageType="analysis",commentText=comment,cityId=cId)
+    city = City.query.filter_by(cityName=city).first()
+    new_comment = Comment(pageType="analysis",commentText=comment,cityId=city.cityId)
 
     db.session.add(new_comment)
     db.session.commit()
