fd8eb5c551b7fbeafc41b3c6a667e7bede04b196
vol2468 <vol2468@student.ubc.ca>
2024-04-03 16:13:07 -0700

    Fixed the scatter plot

--- DashboardProject/dashboard.py
+++ DashboardProject/dashboard.py
@@ -28,21 +28,8 @@ def get_least10_data(user_date):
     return query
 
 
-def get_pollutant_data(user_date):
-    # Retrieve pollutant ratio for the given date.
-    # Query to calculate mean values of pollutants across all cities on the given date
-    query = db.session.query(
-                        func.avg(Pollutant.O3Mean).label('avgO3Mean'),
-                        func.avg(Pollutant.COMean).label('avgCOMean'),
-                        func.avg(Pollutant.SO2Mean).label('avgSO2Mean'),
-                        func.avg(Pollutant.NO2Mean).label('avgNO2Mean'))\
-                        .filter(Pollutant.date == user_date)\
-                        .one()
-    return query
-
-
 def get_aqi_population(user_date):
-    # Retrieve population and mean AQI for the given date.
+    """ Retrieve population and mean AQI for the given date. """
     # Query to join pollutant and city tables, calculate mean AQI, and group by city
     query = db.session.query(City.cityName, City.population, func.avg((Pollutant.O3AQI + Pollutant.COAQI + Pollutant.SO2AQI + Pollutant.NO2AQI) / 4).label('meanAQI'))\
                         .join(Pollutant, City.cityId == Pollutant.cityId)\
@@ -51,4 +38,17 @@ def get_aqi_population(user_date):
                         .order_by(func.avg((Pollutant.O3AQI + Pollutant.COAQI + Pollutant.SO2AQI + Pollutant.NO2AQI) / 4).desc())\
                         .limit(30)\
                         .all()
+    return query
+
+
+def get_pollutant_data(user_date):
+    """ Retrieve pollutant ratio for the given date. """
+    # Query to calculate mean values of pollutants across all cities on the given date
+    query = db.session.query(
+                        func.avg(Pollutant.O3Mean).label('avgO3Mean'),
+                        func.avg(Pollutant.COMean).label('avgCOMean'),
+                        func.avg(Pollutant.SO2Mean).label('avgSO2Mean'),
+                        func.avg(Pollutant.NO2Mean).label('avgNO2Mean'))\
+                        .filter(Pollutant.date == user_date)\
+                        .one()
     return query
