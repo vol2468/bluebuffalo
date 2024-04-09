b0f57c95f72045c48ccc87b1d1302793cbb971d7
joyfuluj <joyumejiego@yahoo.com>
2024-04-09 01:03:53 -0700

    updated testing file

--- DashboardProject/testing/test_project.py
+++ DashboardProject/testing/test_project.py
@@ -11,24 +11,24 @@ def test_analysiscomment(client, app):
                                                     "city":"Phoenix"})
 
     with app.app_context():
-        assert Comment.query.count() == 4
+        assert Comment.query.count() == 3
         assert Comment.query.first().commentText == "It was good."
 
 def test_latitude(client):
     response = client.post("/latitude", data={"city": "Phoenix"})
-    expected_value = "33.4670184857964"
+    expected_value = "33.46701849"
     assert response.data.decode("utf-8") == expected_value
 
 def test_longitude(client):
     response = client.post("/longitude", data={"city": "Phoenix"})
-    expected_value = "-112.045944692803"
+    expected_value = "-112.0459447"
     assert response.data.decode("utf-8") == expected_value
 
 def test_mean_value(client):
     response = client.post("/analysisMean", data={"city": "Phoenix"})
     decoded_data = response.data.decode("utf-8")
     actual_values = [float(value) for value in decoded_data[1:-2].split(',')]
-    expected_values = [0.02884219191154162, 0.5274592119054846, 1.4354918684489446, 19.722046015828575]
+    expected_values = [0.028842191911541974, 0.5274592119054826, 1.4354918684489522, 19.722046015828546]
     assert actual_values == expected_values
 
 def test_total_value(client):
